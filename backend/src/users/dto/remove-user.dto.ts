e9bed85ee888401f94c9f18496ae00da71c3e52f
Bolotinhas <glodsonborges@gmail.com>
2026-06-07 13:12:11 -0300

    fix: fix error message in remove-user.dto for users management

--- backend/src/users/dto/remove-user.dto.ts
+++ backend/src/users/dto/remove-user.dto.ts
@@ -2,7 +2,7 @@ import { IsString, MinLength, MaxLength, IsNotEmpty } from "class-validator";
 
 export class RemoveUserDto {
  @IsString()
- @MinLength(3, {message: 'A senha deve ter pelo menos 3 caracteres. Construa outra senha.',})
+ @MinLength(3, {message: 'A senha deve ter pelo menos 3 caracteres.',})
  @MaxLength(20, {message: 'A senha não pode ter mais de 20 caracteres.'})
  @IsNotEmpty({message: 'O campo senha não pode estar vazio.'})
   password: string;
